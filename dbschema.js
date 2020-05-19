0884aa3eccd72cfa1632c5bfdec5cce4f6c72788
demkantor <demkantor@gmail.com>
2020-05-18 23:24:05 -0500

    adding schema for refrence

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    disregards: [
+        {
+            userHandle: 'user',
+            body: 'disregard body',
+            createdAt: '2020-05-14T11:46:01.018Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
